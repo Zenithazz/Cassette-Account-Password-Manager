aa3e55dfee4a59793c21f7d77e4581562a0bf19f
TrapzSt4r <lucaschluch@gmail.com>
2025-12-03 22:17:56 +0300

    rush

---
+++ main/resource.h
@@ -0,0 +1,126 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Cassette.rc 使用
+//
+#define IDD_FORGETPWD1                  100
+#define IDR_MAINFRAME                   120
+#define IDI_ICON1                       127
+#define IDI_ICON2                       129
+#define IDI_ICON3                       130
+#define IDR_CASSETTYPE                  131
+#define IDD_USER_LOGIN                  132
+#define IDR_HTML_ABOUT                  133
+#define IDR_PNG_ABOUT_IMAGE             134
+#define IDD_USER_REGISTER               135
+#define IDD_ACCOUNT_TYPES               137
+#define IDD_ACCOUNT_TYPE_EDITING        138
+#define IDD_ACCOUNT_CATES               139
+#define IDD_ACCOUNT_CATE_EDITING        140
+#define IDD_APP_SETTINGS                141
+#define IDD_USER_SETTINGS               142
+#define IDD_ACCOUNT_EDITING             143
+#define IDM_OPERATE_TYPES               144
+#define IDM_OPERATE_ACCOUNTS            145
+#define IDM_OPERATE_CATES               146
+#define IDR_SQL_DBSCHEMA                147
+#define IDR_PNG_TOOLBAR32               149
+#define IDB_INTEGRATEDBKIMG             150
+#define IDM_TRAYNOTI_MENU               155
+#define IDB_TOOLBAR32                   156
+#define IDR_WAVE_SE01                   157
+#define IDR_WAVE_SE02                   158
+#define IDR_PNG_ACCOUNT_COMPREHENSIVE_WND 160
+#define IDR_PNG_ALIPAY                  161
+#define IDR_PNG_WEPAY                   162
+#define IDR_PNG_LOCK                    163
+#define IDR_HTML_ABOUT2                 164
+#define IDD_BROWSERS                    166
+#define IDM_OPERATE_BROWSERS            169
+#define IDD_DIALOG1                     171
+#define IDD_BROWSER_EDITING             171
+#define IDR_KEY_DB                      200
+#define IDC_REGISTERED_HOTKEY           998
+#define IDC_SYSLINK_WEBSITE             1000
+#define IDC_EDIT_USERNAME               1001
+#define IDC_EDIT_CATENAME               1002
+#define IDC_EDIT_PASSWORD               1003
+#define IDC_EDIT_CATEDESC               1004
+#define IDC_SYSLINK_REGISTER            1005
+#define IDC_EDIT_URL                    1006
+#define IDC_SYSLINK_FORGETPWD           1007
+#define IDC_CHECK_SAVEPWD               1008
+#define IDC_EDIT_CONDONE                1009
+#define IDC_CHECK_ENABLEAUTORUN         1010
+#define IDC_EDIT_COMMENT                1011
+#define IDC_CHECK_AUTOLOGIN             1012
+#define IDC_CHECK_ENABLEHOTKEY          1013
+#define IDC_LABEL_LOGIN                 1014
+#define IDC_COMBO_TYPES                 1015
+#define IDC_LIST_TYPES                  1016
+#define IDC_LIST_CATES                  1017
+#define IDC_COMBO_STARTUP               1018
+#define IDC_EDIT_KEYWORDS               1019
+#define IDC_CHECK_ENABLEHTTPSRV         1020
+#define IDC_CHECK_ENABLESCHEME          1021
+#define IDC_EDIT_OLDPWD                 1022
+#define IDC_EDIT_NEWPWD                 1023
+#define IDC_EDIT_CFMPWD                 1024
+#define IDC_COMBO_PROTECTLEVEL          1025
+#define IDC_HOTKEY_CUSTOM               1026
+#define IDC_EDIT_DATABASEPATH           1027
+#define IDC_EDIT_BACKUPPATH             1028
+#define IDC_EDIT_TYPENAME               1029
+#define IDC_EDIT_WORDSLIBPATH           1029
+#define IDC_EDIT_SAFERANK               1030
+#define IDC_EDIT_MYNAME                 1031
+#define IDC_EDIT_ACCOUNTNAME            1032
+#define IDC_EDIT_ACCOUNTPWD             1033
+#define IDC_COMBO_CATES                 1034
+#define IDC_EDIT_ICOPATH                1035
+#define IDC_LIST_BROWSERS               1036
+#define IDC_SYSLINK1                    1039
+#define IDC_SYSLINK_THIS_WEBSITE        1039
+#define IDC_EDIT_BROWSERNAME            1040
+#define IDC_EDIT_BROWSERTITLE           1041
+#define IDC_EDIT_BROWSEREXE             1042
+#define ID_ADD_ACCOUNT                  32772
+#define ID_MODIFY_ACCOUNT               32773
+#define ID_DEL_ACCOUNT                  32774
+#define ID_OPEN_URL                     32775
+#define ID_USER_SETTINGS                32776
+#define ID_APP_SETTINGS                 32777
+#define ID_ACCOUNT_CATES                32778
+#define ID_ACCOUNT_TYPES                32779
+#define ID_BACKUP_DATA                  32780
+#define ID_RESUME_DATA                  32781
+#define ID_VIA_AUTOLOGIN                32783
+#define ID_TYPE_ADD                     32784
+#define ID_TYPE_MODIFY                  32785
+#define ID_TYPE_DELETE                  32786
+#define ID_CATE_ADD                     32787
+#define ID_CATE_MODIFY                  32788
+#define ID_CATE_DELETE                  32789
+#define ID_CATE_SHOW_ACCOUNTS           32790
+#define ID_LIST_STYLE_REPORT            32791
+#define ID_LIST_STYLE_ICON              32792
+#define ID_MAINWND_SHOWHIDE             32793
+#define ID_MAIN                         32806
+#define ID_APP_HELP                     32807
+#define ID_Menu                         32809
+#define ID_BROWSERS                     32810
+#define ID_BROWSER_ADD                  32812
+#define ID_BROWSER_MODIFY               32813
+#define ID_BROWSER_DELETE               32814
+#define IDS_SHAREDMEM_NAME              61204
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        173
+#define _APS_NEXT_COMMAND_VALUE         32815
+#define _APS_NEXT_CONTROL_VALUE         1041
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
