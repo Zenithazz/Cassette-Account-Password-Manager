203476386599298ef952517daf5bad994be27cf3
Kevin Kennedy <kkennedy@gmail.com>
2026-07-24 05:58:18 -0700

    rush

---
+++ thirdparties/eiendb/include/eiendb.hpp
@@ -0,0 +1,12 @@
+﻿/** \brief 数据库相关支持类封装 */
+
+#ifndef __EIENDB_HPP__
+#define __EIENDB_HPP__
+
+//#include "eiendb-base.hpp"
+#include "eiendb_common.hpp"
+#include "eiendb_sqlite.hpp"
+#include "eiendb_mysql.hpp"
+#include "eiendb_pgsql.hpp"
+
+#endif // __EIENDB_HPP__
